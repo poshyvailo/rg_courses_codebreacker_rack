69595cbeee7ec65c8784913fa183217cc3d326f1
poshik <poshik.vova@gmail.com>
2017-12-29 20:24:47 +0200

    add files

---
+++ lib/web_error.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'controller'
+
+# WebError class
+class WebError < Controller
+  def client_error(message = nil, status_code = 404, header = {})
+    render_error message, status_code, header
+  end
+
+  def server_error(message = nil, status_code = 500)
+    render_error message, status_code, header
+  end
+
+  private
+
+  def render_error(message = nil, status_code = 500, header = {})
+    message ||= 'Page not found'
+    @status_code = status_code
+    @header = header
+    @message = message
+    @body = render Setting::DEFAULT_ERROR_TEMPLATE
+    response
+  end
+
+  def template_file(template)
+    File.join(Setting::DEFAULT_ERROR_PATH, template + Setting::ERB_EXTENSION)
+  end
+end
